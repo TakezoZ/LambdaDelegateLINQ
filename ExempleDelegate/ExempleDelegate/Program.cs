6f3dd0204f8da42b5421261010ac05fea5f08c1f
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-11-01 13:55:44 -0300

    V2-Introdução a Delegate(Multicast Delegates)

--- ExempleDelegate/ExempleDelegate/Program.cs
+++ ExempleDelegate/ExempleDelegate/Program.cs
@@ -4,17 +4,17 @@ namespace ExempleDelegate
 {
     class Program
     {
-        delegate double BinaryNumericOperation(double n1, double n2);
+        delegate void BinaryNumericOperation(double n1, double n2);
 
         static void Main(string[] args)
         {
             double a = 10;
             double b = 12;
-            // BinaryNumericOperation op = CalculationService.Sum;
-            BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
-            // double result = op(a, b);
-            double result = op.Invoke(a, b);
-            Console.WriteLine(result);
+
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+            
+            op.Invoke(a, b);
         }
     }
 }
