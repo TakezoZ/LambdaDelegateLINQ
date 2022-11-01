81fe148db72e2587519e76abaaafa7116c5db67d
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-11-01 14:20:37 -0300

    Action (Exemplo com ForEach)

---
+++ ExempleAction/ExempleAction/Program.cs
@@ -0,0 +1,31 @@
+﻿using ExempleAction.Entities;
+
+namespace ExempleAction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            Action<Product> act = p => { p.Price += p.Price * 0.10; }; //Quando a função possui corpo mas não retorna nada. Void
+
+            list.ForEach(act);
+            foreach (Product product in list)
+            {
+                Console.WriteLine(product);
+            }
+
+        }
+
+        static void UpdatePrice(Product product)
+        {
+            product.Price += product.Price * 0.10;
+        }
+    }
+}
