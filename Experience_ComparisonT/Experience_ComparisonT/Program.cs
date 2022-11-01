9398f14cb955ed88f73b73a25815a0db3e261e10
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-11-01 12:07:09 -0300

    V1-Exemplo Comparison<T>(Antes de implementar sobrecarga)

---
+++ Experience_ComparisonT/Experience_ComparisonT/Program.cs
@@ -0,0 +1,23 @@
+﻿using Experience_ComparisonT.Entities;
+
+namespace Experience_ComparisonT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            list.Sort();
+
+            foreach (Product product in list)
+            {
+                Console.WriteLine(product);
+            }
+        }
+    }
+}
