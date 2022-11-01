0556a05e87fb8216fdac0016ddf0a52cabeb6210
Marcos Takezo Otta <Takezootta02@gmail.com>
2022-11-01 20:52:45 -0300

    V2-Exercicio de Fixação(Corrigido)

--- ExercicioDeFixacao/ExercicioDeFixacao/Program.cs
+++ ExercicioDeFixacao/ExercicioDeFixacao/Program.cs
@@ -24,26 +24,35 @@ namespace ExercicioDeFixacao
 
             List<Employee> employees = new List<Employee>();
 
-            using (StreamReader sr = File.OpenText(path))
+
+            try
             {
-                while (!sr.EndOfStream)
+                using (StreamReader sr = File.OpenText(path))
                 {
-                    string[] line = sr.ReadLine().Split(',');
-                    string name = line[0];
-                    string email = line[1];
-                    double salary = double.Parse(line[2], CultureInfo.InvariantCulture);
-                    employees.Add(new Employee(name, email, salary));
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        string email = line[1];
+                        double salary = double.Parse(line[2], CultureInfo.InvariantCulture);
+                        employees.Add(new Employee(name, email, salary));
+                    }
                 }
+
+                var salaryMoreThanBase = employees.Where(e => e.Salary >= baseSalary)
+                                                  .OrderBy(e => e.Email)
+                                                  .Select(e => e.Email);
+                Print($"Email of people whose salary is more than {baseSalary.ToString("F2", CultureInfo.InvariantCulture)}:", salaryMoreThanBase);
+
+                var sum = employees.Where(e => e.Name[0] == 'M')
+                                   .Sum(e => e.Salary);
+                Console.Write($"Sum of salary of people whose name starts with 'M': {sum.ToString("F2", CultureInfo.InvariantCulture)}");
             }
-
-            var salaryMoreThanBase = employees.Where(e => e.Salary >= baseSalary)
-                                              .Select(e => e.Email);
-            Print($"Email of people whose salary is more than {baseSalary.ToString("F2", CultureInfo.InvariantCulture)}:", salaryMoreThanBase);
-
-            var sum = employees.Where(e => e.Name[0] == 'M')
-                               .Sum(e => e.Salary);
-            Console.Write($"Sum of salary of people whose name starts with 'M': {sum.ToString("F2", CultureInfo.InvariantCulture)}");
-
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
